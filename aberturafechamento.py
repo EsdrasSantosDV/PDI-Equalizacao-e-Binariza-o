6c901fcc70e661b8a50b5f417f74b2343db3f15a
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-09-30 20:53:37 -0300

    teste

---
+++ aberturafechamento.py
@@ -0,0 +1,19 @@
+
+import cv2
+import numpy as np
+img = cv2.imread('./dado.jpg')
+
+kernel = np.ones((3,3),np.uint8)  
+
+erosao=cv2.erode(img,kernel,iterations=14)
+
+cv2.imwrite("erosao.jpg",erosao)
+
+
+dilatacao=cv2.dilate(img,kernel,iterations=14)
+
+cv2.imwrite("dilatacao.jpg",dilatacao)
+
+dilatacao=cv2.dilate(img,kernel,iterations=14)
+
+cv2.imwrite("dilatacao.jpg",dilatacao)
